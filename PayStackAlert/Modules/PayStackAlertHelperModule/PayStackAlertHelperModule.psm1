d67bea7113d2910a9137941857b53146163b335f
olusola-adio <olusola_adio@hotmail.com>
2020-09-29 19:31:22 +0100

    added subscription create

--- PayStackAlert/Modules/PayStackAlertHelperModule/PayStackAlertHelperModule.psm1
+++ PayStackAlert/Modules/PayStackAlertHelperModule/PayStackAlertHelperModule.psm1
@@ -221,7 +221,9 @@ boolean.
         $uniqueId = $Alert.data.invoice_code
     } elseif ("invoice.create" -eq $Alert.event){
         $uniqueId = $Alert.data.invoice_code
-    } elseif ("invoice.disable" -eq $Alert.event){
+    } elseif ("subscription.disable" -eq $Alert.event){
+        $uniqueId = $Alert.data.plan.plan_code
+    } elseif ("subscription.create" -eq $Alert.event){
         $uniqueId = $Alert.data.plan.plan_code
     } else {
         $uniqueId = $Alert.data.id
